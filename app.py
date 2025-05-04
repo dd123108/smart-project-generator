64aeb0a569b78a89b39ec01476396f40f7323542
dd123108 <man18mohan@gmail.com>
2025-05-05 03:56:33 +0530

    Fixed port binding for Render

--- app.py
+++ app.py
@@ -302,6 +302,10 @@ def add_background_image(slide, image_path, prs):
 def home():
     return render_template('index.html')
 
+    if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 @app.route('/generate', methods=['POST'])
 def generate():
     name = request.form['name']
